bb1d0366c3259105438879fc0a1a78ea463e444e
Fabro <fabromariaclara@gmail.com>
2025-02-10 15:55:05 -0300

    feat: connecting to the db

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 const dbURL = process.env.DATABASE_URL;
 
+//acho que eu tenho tdah...
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(dbURL)
+    .then((result) => console.log('database is connected'))
+    .catch((err) => console.log('error at connecting with the db',err));
