b1657190ff0092fcf3c83ba62534a5236533d5c8
Fabro <fabromariaclara@gmail.com>
2025-02-26 16:34:15 -0300

    refactor: improve code organization and remove unused code

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
+require('dotenv').config();
 
 const authRouter = require('./routes/authRoutes');
 const taskRouter = require('./routes/taskRoutes');
 const { checkUser } = require('./middleware/authMiddleware');
 
-require('dotenv').config();
 const dbURL = process.env.DATABASE_URL;
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(express.static('public'));
@@ -22,10 +21,15 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(dbURL)
-    .then((result) => app.listen(port))
+    .then((result) => {
+        console.log('successfully connected with the db');
+        app.listen(port, () =>{
+            console.log(`server listening at ${port}`)
+        });
+    })
     .catch((err) => console.log('error at connecting with the db', err));
 
-app.use(checkUser);
+app.use("*", checkUser);
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -34,4 +38,7 @@ app.get('/', (req, res) => {
 app.use('/auth', authRouter);
 app.use('/task', taskRouter); 
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.use((err, req, res, next) =>{
+    console.error(err.stack);
+    res.status(500).render('error', {message: 'something went wrong :/'});
+});
